88b960e9cfee3548aa7dd5480006aeee6f8fbf06
sudhkarmalle <35170902+madhusudhakar111@users.noreply.github.com>
2019-01-18 15:10:28 +0530

    Update InstructionReorder.java

--- InstructionReorder.java
+++ InstructionReorder.java
@@ -5,10 +5,10 @@ public class InstructionReorder {
 	public static void main(String[] args) {
 		int a = 1;		
 		int b = 2;
-		b++;
-		System.out.println("Helloworld");
 		a++;
-	
+		b++;
+		
+			
 		
 	}
 

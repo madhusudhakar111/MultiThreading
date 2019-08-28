d00bea219b6c7169f18c4c4e9fe4ee6e67df7e02
SudhakarMalle <sudhakar_malle@crgl-thirdparty.com>
2019-08-28 14:33:29 +0530

    completeable feature

---
+++ concurency/completableFuture/CompletableFutureExample.java
@@ -0,0 +1,67 @@
+package concurency.completableFuture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CompletableFutureExample {
+	
+	private void runAsync() throws InterruptedException, ExecutionException {
+		
+		CompletableFuture<Void> future = CompletableFuture.runAsync(new Runnable() {
+		    @Override
+		    public void run() {
+		    	try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		        System.out.println("I'll run in a separate thread than the main thread.");
+		    }
+		});
+		future.get();
+	}
+	
+	private Integer getNumber(int number) {
+		return number;
+	}
+	
+	private void supplyAsync() throws InterruptedException, ExecutionException {
+		
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+		AtomicInteger integer = new AtomicInteger(0);
+		List<CompletableFuture> lst = new ArrayList();
+		
+		for(int i=0; i<10;i++ ) {
+		lst.add(
+					CompletableFuture.supplyAsync(
+							() -> { return getNumber(integer.incrementAndGet())+""; }, 	executor)
+			   );
+		}
+						
+		for(CompletableFuture future: lst) {
+			System.out.println(future.get());
+		}
+		
+		executor.shutdown();
+	}
+	
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		
+		
+		CompletableFutureExample obj = new CompletableFutureExample();
+		
+		//obj.runAsync();
+		obj.supplyAsync();
+	
+		
+	}
+	
+	
+}
